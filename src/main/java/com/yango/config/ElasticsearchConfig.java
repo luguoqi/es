aa691cc7e5b75d227fbb7eb72343b75118d80411
lugq <SSH>
2021-01-11 23:43:43 +0800

    测试提交

--- src/main/java/com/yango/config/ElasticsearchConfig.java
+++ src/main/java/com/yango/config/ElasticsearchConfig.java
@@ -1,6 +1,8 @@
 package com.yango.config;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.HttpAsyncResponseConsumerFactory;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
@@ -10,9 +12,7 @@ import static org.elasticsearch.client.RestClient.*;
 
 /**
  * @Description
- * 1、导入依赖
- * 2、编写配置。给容器中注入一个 RestHighLevelClient   参考https://www.elastic.co/guide/en/elasticsearch/client/java-rest/current/java-rest-high-getting-started-initialization.html
- *
+
  * @Author lugq
  * @Email lugq@yango.com.cn
  * @Date 2021/1/10 0010 23:08
@@ -23,6 +23,9 @@ public class ElasticsearchConfig {
 
     /**
      * 如果ES有多个可以直接指定
+     * 1、导入依赖
+     * 2、编写配置。给容器中注入一个 RestHighLevelClient
+     *      参考 https://www.elastic.co/guide/en/elasticsearch/client/java-rest/current/java-rest-high-getting-started-initialization.html
      */
     @Bean
     public RestHighLevelClient esRestClient() {
@@ -30,6 +33,23 @@ public class ElasticsearchConfig {
                 builder(//new HttpHost("localhost", 9200, "http"),
                         new HttpHost("192.168.136.135", 9200, "http")));
         return client;
+    }
+
+    /**
+     * RequestOptions 对所有请求进行统一设置，官网建议做成单实例，供所有请求使用
+     * 请求的通用设置项(例如ES添加了安全访问规则，则可以设置安全的令牌信息)
+     * 参考：https://www.elastic.co/guide/en/elasticsearch/client/java-rest/current/java-rest-low-usage-requests.html#java-rest-low-usage-request-options
+     * 然后测试ES的增删改查
+     */
+    public static final RequestOptions COMMON_OPTIONS;//通用设置项
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+//        builder.addHeader("Authorization", "Bearer " + TOKEN);//每次请求之前带上请求头信息，比如令牌
+        builder.setHttpAsyncResponseConsumerFactory(
+                new HttpAsyncResponseConsumerFactory
+                        .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024)
+        );//异步相关，定义响应消费者
+        COMMON_OPTIONS = builder.build();
     }
 
 
--- src/test/java/com/yango/EsApplicationTests.java
+++ src/test/java/com/yango/EsApplicationTests.java
@@ -1,9 +1,16 @@
 package com.yango;
 
+import com.yango.config.ElasticsearchConfig;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 
 @SpringBootTest
@@ -15,6 +22,31 @@ class EsApplicationTests {
     @Test
     public void contextLoads() {
         System.out.println(client);
+    }
+
+    /**
+     * 三种形式，字符串、map、对象(一般转成json)、使用source将key-vale存储
+     * https://www.elastic.co/guide/en/elasticsearch/client/java-rest/current/java-rest-high-document-index.html
+     * 两种执行方式
+     * 同步：Synchronous execution
+     *  IndexResponse indexResponse = client.index(request, RequestOptions.DEFAULT);
+     * 异步：Asynchronous executionedit
+     *  client.indexAsync(request, RequestOptions.DEFAULT, listener);   listener：成功后调用的方法
+     * 使用kibana查询posts  GET posts/_search
+     */
+    @Test
+    public void insertData() throws IOException {
+        IndexRequest request = new IndexRequest("posts");
+        request.id("1");
+        String jsonString = "{" +
+                "\"user\":\"kimchy\"," +
+                "\"postDate\":\"2013-01-30\"," +
+                "\"message\":\"trying out Elasticsearch\"" +
+                "}";
+        request.source(jsonString, XContentType.JSON);//注意json时要传内容类型
+//        执行操作并响应数据
+        IndexResponse indexResponse = client.index(request, ElasticsearchConfig.COMMON_OPTIONS);
+        System.out.println(indexResponse);
     }
 
 }
